e8098290002cdcfd7ff7dd42bfff3fe33b1e5ccb
newfivefour <newfivefour@newfivefour.com>
2019-02-26 12:26:46 +0100

    Added error handler.

--- app.js
+++ app.js
@@ -178,6 +178,22 @@ app.get("/auth/logout", (req, res) => {
   res.json({ message: 'You are out!' })
 })
 
+app.use((err, req, res, next) => {
+  console.error("----- An error happened -----")
+  console.error(err)
+
+  // only render if the error ocurred before sending the response
+  if (!res.headersSent) {
+    res.status(err.status || 500)
+
+    // A limited amount of information sent in production
+    if (process.env.NODE_ENV === 'production')
+      res.json(err)
+    else
+      res.json(JSON.parse(JSON.stringify(err, Object.getOwnPropertyNames(err))))
+  }
+})
+
 // dogs stuff
 
 var dogs = [
